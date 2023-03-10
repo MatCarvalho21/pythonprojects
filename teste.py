dc1dc6a9b46ca440fd01105a0c67fe9843dfd14a
Matheus <matheuscarvalho210404@outlook.com>
2023-03-10 08:11:03 -0300

    Arredondamento

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-`Baskara
+Báskara 
 a = input("Qual o seu coeficiente a? ")
 b = input("Qual o seu segundo coeficiente? O b. ")
 c = input("Qual o seu último coeficiente? ")
@@ -7,8 +7,8 @@ y = (b^2) - (4*a*c)
 y2 = y^(1/2)
 
 if y2>0:
-    x1 = (-b + y2)/2*a 
-    x2 = (-b - y2)/2*a
+    x1 = round(((-b + y2)/2*a), 2)
+    x2 = round(((-b - y2)/2*a), 2)
     print("As suas raízes são:", y, y2)
 else: 
     print("A sua equação não possui raízes reais.")
