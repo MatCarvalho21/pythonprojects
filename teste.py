c7d8ac40a50864419e0643c0b8c0b9b1e8870ab2
Matheus <matheuscarvalho210404@outlook.com>
2023-03-10 07:44:46 -0300

    Primeiro

---
+++ teste.py
@@ -0,0 +1,13 @@
+a = input("Qual o seu coeficiente a? ")
+b = input("Qual o seu segundo coeficiente? O b. ")
+c = input("Qual o seu último coeficiente? ")
+
+y = (b^2) - (4*a*c)
+y2 = y^(1/2)
+
+if y2>0:
+    x1 = (-b + y2)/2*a 
+    x2 = (-b - y2)/2*a
+    print("As suas raízes são:", y, y2)
+else: 
+    print("A sua equação não possui raízes reais.")
