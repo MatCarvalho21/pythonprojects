bf53bcce15f62cd4fa249a390fcac319e6d2b6e2
MATHEUS <matheuscarvalho210404@outlook.com>
2023-03-11 20:18:44 -0300

    Modificações Estéticas

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
-Báskara 
+########## RESOLUÇÃO DE EQ. DO SEGUNDO GRAU ########## 
+print("Vamos começar!! Informe os coeficientes da sua equação. Caso não forem números inteiros, coloque um ponto para iniciar as casas decimais.")
 a = input("Qual o seu coeficiente a? ")
 b = input("Qual o seu segundo coeficiente? O b. ")
 c = input("Qual o seu último coeficiente? ")
@@ -11,4 +12,4 @@ if y2>0:
     x2 = round(((-b - y2)/2*a), 2)
     print(f"As suas raízes são: {x1:,.2f} e {x2:,.2f}")
 else: 
-    print("A sua equação não possui raízes reais.")
+    print("A sua equação não possui raízes reais. Confira os valores.")
