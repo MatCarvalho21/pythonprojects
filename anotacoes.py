ae477fba270f289a9127139b6ed57e3101ed3fd0
Matheus <matheuscarvalho210404@outlook.com>
2023-03-11 22:24:47 -0300

    anotações de comandos do git

--- anotacoes.py
+++ anotacoes.py
@@ -1,1 +1,19 @@
 #################### ANOTAÇÕES IMPORTANTES ####################
+"git mv nome.antigo nome.novo" - comando para renomear arquivos 
+"git config --global user.name nome.de.usuário" - comando para mudar ou colocar nome de usuário no git bash
+"git config --global user.email email.de.usuário" - comando para mudar ou colocar o email do usuário no git
+"git congif list" - comando para listar todas as configurações do gitbash no momento
+"mkdir nome.reositório.novo" - comando para criar novo diretório
+"cd nome.diretório" - comando para entrar no diretório 
+"cd \.." - comando para voltar uma pasta
+"git clone URL.diretorio.remoto" - vai clonar seu diretório remoto no diretório local, permitindo as alteraçẽos 
+"git add" - comando para que o gitbash trackeie o seu arquivo permitindo o commit
+"git commit -a" - vai commitar todos os arquivos (salvar alterações)
+"git commmit -m 'mensagem de commit' " - vai commitar todos, mas com uma mensagem direta no terminal 
+"git push" - manda as alterações para o repositório remoto (github no caso)
+"git pull" - vai trazer as alterações que foram feitas no repositório remoto 
+"git branch nome.branch" - comando que permite criar uma ramificação do diretório parafazer alterações em segurança
+"git switch nome.branch" - comando que permite transitar entre os branchs e o ramo main
+"git merge nome.branch" - vai trazer todas as alterações feitas no branch para o ramo main
+"git status" - vai mostrar os aquivos do repositório e a situação deles 
+"git log" - mostra os ultimos commits feitos 
