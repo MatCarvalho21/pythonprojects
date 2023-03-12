d76d921ecdabe4ee2ad0253b7054b5128ae3fa47
Matheus <matheuscarvalho210404@outlook.com>
2023-03-12 15:49:41 -0300

    sorteador de ordem

---
+++ sorteadorordem.py
@@ -0,0 +1,9 @@
+########## SORTEADOR DE ORDEM ##########
+import random 
+a1 = input("Digite o nome do primeiro aluno. ")
+a2 = input("Digite o nome do segundo. ")
+a3 = input("Do terceiro. ")
+a4 = input("Do quarto agora. ")
+lista = [a1, a2, a3, a4]
+lista2 = random.shuffle(lista)
+print("A ordem escolhida foi: {}.".format(lista2))
