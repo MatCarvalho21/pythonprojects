6feb97f59aca209729bdb42df2337fb30837d14c
Matheus <matheuscarvalho210404@outlook.com>
2023-03-12 14:20:13 -0300

    Correção da Infinitude da Tangente

--- trigonometria.py
+++ trigonometria.py
@@ -6,4 +6,7 @@ a = r*f
 t = math.tan(a)
 s = math.sin(a)
 c = math.cos(a)
-print(f"O valor do seno do ângulo {0} é {s:,.2f}, do cosseno é {c:,.2f} e da tangente é {t:,.2f}. ".format(a))
+if (t <= 10000):
+    print(f"O valor do seno do ângulo {0} é {s:,.2f}, do cosseno é {c:,.2f} e da tangente é {t:,.2f}. ".format(a))
+else:
+    print(f"O valor do seno do ângulo {0} é {s:,.2f}, do cosseno é {c:,.2f} e da tangente é muito próxima do infinito. ".format(a))
