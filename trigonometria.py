2436c7ae202e4b05dd745ecbaedd6fe47653662d
Matheus <matheuscarvalho210404@outlook.com>
2023-03-12 14:13:45 -0300

    Funções Trigonométricas na Biblioteca Math

---
+++ trigonometria.py
@@ -0,0 +1,7 @@
+########## TRIGONOMETRIA ##########
+import math
+a = float(input("Coloque o valor do seu ângulo em graus. "))
+t = math.tan(a)
+s = math.sin(a)
+c = math.cos(a)
+print(f"O valor do seno do ângulo {0} é {s:,.2f}, do cosseno é {c:,.2f} e da tangente é {t:,.2f}. ".format(a))
